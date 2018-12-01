82718d941c5e294625d6b9219df1c6737a52e125
anange <a.angelakis@outlook.com>
2018-12-01 19:52:47 +0200

    Add part 2 of day01

---
+++ src/bin/day01b.rs
@@ -0,0 +1,24 @@
+extern crate aoc2018;
+
+use std::collections::HashSet;
+use aoc2018::files::read_from_file;
+
+pub fn main() {
+    let input = read_from_file("input/day01a.txt");
+    let mut freqs: Vec<i64> = input.split('\n')
+                           .map(|x| x.parse::<i64>().unwrap_or_default())
+                           .collect();
+    freqs.pop();
+    let mut found_freqs = HashSet::new();
+    let mut current_freq: i64 = 0;
+    loop {
+        for shift in &freqs {
+            current_freq += shift;
+            if found_freqs.contains(&current_freq) {
+                println!("Duplicate freq found {}", current_freq);
+                return
+            }
+            found_freqs.insert(current_freq);
+        }
+    }
+}
