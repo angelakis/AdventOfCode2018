1b231ad320fc66285f7681ead352a26907cba748
anange <a.angelakis@outlook.com>
2018-12-15 16:12:58 +0200

    Finish optimized version for day09b

--- src/bin/day09b.rs
+++ src/bin/day09b.rs
@@ -1,10 +1,36 @@
-use std::cell::RefCell;
-use std::rc::Rc;
+use std::collections::HashMap;
 
+#[derive(Clone)]
 pub struct Node {
     marble: usize,
-    previous: RefCell<Rc<Option<Node>>>,
-    next: RefCell<Rc<Option<Node>>>,
+    previous: usize,
+    next: usize,
+}
+
+impl Node {
+    pub fn blank() -> Node {
+        Node {marble: 0, previous: 0, next: 0}
+    }
+    pub fn new(marble: usize, previous: usize, next: usize) -> Node {
+        Node {marble: marble, previous: previous, next: next}
+    }
+}
+
+pub fn insert_after(nodes: &mut HashMap<usize, Node>, spot: usize, marble: usize) {
+    let before = nodes.get(&spot).unwrap().clone();
+    let after = nodes.get(&before.next).unwrap().clone();
+    nodes.insert(marble, Node::new(marble, before.marble, after.marble));
+    nodes.insert(before.marble, Node::new(before.marble, before.previous, marble));
+    nodes.insert(after.marble, Node::new(after.marble, marble, after.next));
+}
+
+pub fn remove_at(nodes: &mut HashMap<usize, Node>, spot: usize) {
+    let node = nodes.get(&spot).unwrap().clone();
+    let previous = nodes.get(&node.previous).unwrap().clone();
+    let next = nodes.get(&node.next).unwrap().clone();
+    nodes.remove(&spot);
+    nodes.insert(previous.marble, Node::new(previous.marble, previous.previous, node.next));
+    nodes.insert(next.marble, Node::new(next.marble, node.previous, next.next));
 }
 
 pub fn main() {
@@ -13,53 +39,31 @@ pub fn main() {
         .trim()
         .split(' ')
         .collect();
-    let (player_num, marbles_num): (usize, usize) = 
+    let (player_num, marbles_num): (usize, usize) =
         (input[0].parse().unwrap(), input[6].parse().unwrap());
-    let mut first = Node{marble: 0, previous: RefCell::new(Rc::new(None)), next: RefCell::new(Rc::new(None))};
-    let first_link = Rc::new(Some(first));
-    let second = Node{marble: 1, previous: RefCell::new(Rc::clone(&first_link)), next: RefCell::new(Rc::clone(&first_link))};
-    let second_link = Rc::new(Some(second));
-    first.previous = RefCell::new(Rc::clone(&second_link));
-    first.next = RefCell::new(Rc::clone(&second_link));
+    let mut nodes: HashMap<usize, Node> = HashMap::new();
+    nodes.insert(0, Node::new(0, 1, 1));
+    nodes.insert(1, Node::new(1, 0, 0));
     let mut scores = vec!(0; player_num);
-    let mut designated = second_link;
-    for marble in 2..marbles_num {
+    let mut designated = 1;
+    for marble in 2..100 * marbles_num {
         if marble % 23 != 0 {
-            let next_one = &designated;
+            let designated_node = nodes.get(&designated).unwrap().clone();
+            let spot = designated_node.next;
+            insert_after(&mut nodes, spot, marble);
+            designated = marble;
         } else {
+            let mut to_remove = designated;
+            for _ in 0..7 {
+                let current = nodes.get(&to_remove).unwrap();
+                to_remove = current.previous;
+            }
+            let node_to_remove = nodes.get(&to_remove).unwrap().clone();
+            remove_at(&mut nodes, to_remove);
+            let cur_player = marble % player_num;
+            scores[cur_player] += marble + to_remove;
+            designated = node_to_remove.next;
         }
     }
-    // for marble in 5..marbles_num {
-    //     if marble % 23 != 0 {
-    //         let new_position = designated + 2;
-    //         if new_position == field_len {
-    //             field.push(marble);
-    //             designated = field_len;
-    //         } else if new_position == field_len + 1 {
-    //             field.insert(1, marble);
-    //             designated = 1;
-    //         } else {
-    //             field.insert(new_position, marble);
-    //             designated = new_position;
-    //         }
-    //         field_len += 1;
-    //     } else {
-    //         let cur_player = marble % player_num;
-    //         let to_remove = {
-    //             if designated >= 7 {
-    //                 designated - 7
-    //             } else {
-    //                 field_len + designated - 7
-    //             }
-    //         };
-    //         scores[cur_player] += marble + field[to_remove];
-    //         field.remove(to_remove);
-    //         designated = to_remove;
-    //         field_len -= 1;
-    //     }
-    //     if marble % (marbles_num) == 0 {
-    //         println!("{}% done", marble / (marbles_num));
-    //     }
-    // }
     println!("Winner scored {}", scores.iter().max().unwrap());
 }
